18e47c48d9a9449d8406b7fe13d7c99a06f89732
Yuanuox <yuanuox@outlook.com>
2023-11-28 10:16:47 +0800

    修复同类型同ID无法匹配到的问题

--- src/main/java/org/appxi/cbeta/Book.java
+++ src/main/java/org/appxi/cbeta/Book.java
@@ -1,7 +1,10 @@
 package org.appxi.cbeta;
 
+import org.appxi.util.StringHelper;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Book extends org.appxi.book.Book {
     public String number;
@@ -12,6 +15,9 @@ public class Book extends org.appxi.book.Book {
     public String authorInfo() {
         if (!this.hasAttr(AK_AUTHORS)) {
             BookHelper.parseBookAuthorInfo(this);
+            if (StringHelper.isBlank(this.authorInfo)) {
+                this.authorInfo = String.join(", ", this.authors);
+            }
             this.attr(AK_AUTHORS, true);
         }
         return this.authorInfo;
@@ -35,4 +41,18 @@ public class Book extends org.appxi.book.Book {
     public Book ofBook() {
         return new Book();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(path, book.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, path);
+    }
+
 }
